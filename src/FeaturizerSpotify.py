6a5c0ed0f88375d15d6e90bf4e53ceb918e7b209
j-abc <ubuntu@ip-172-31-49-64.us-west-2.compute.internal>
2019-10-03 17:58:39 +0000

    spotify feats now only variables w/ vals 0- 1

--- src/FeaturizerSpotify.py
+++ src/FeaturizerSpotify.py
@@ -5,9 +5,11 @@ from .SpotifyConnector import SpotifyConnector
 class FeaturizerSpotify(Featurizer):
     def __init__(self):
         Featurizer.__init__(self, name = 'SpotifyMusicality')
-        self.feature_keys  = []
+        self.feature_keys  = ['danceability', 'energy', 'loudness', 'mode','speechiness','acousticness','instrumentalness', 'liveness','valence']
+
+        # all features on scale [0, 1], removed mode, key, and tempo
         self.connection2spotify = SpotifyConnector()
         
     def calculate_features_for_track(self, track_id):
         out_features = self.connection2spotify.instance.audio_features(track_id)[0]
-        return out_features
+        return {key:item for key, item in out_features.items() if key in self.feature_keys}
